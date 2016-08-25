3160475dfdad6a35f71b982795bc949bbeaf9ebf
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-08-25 13:08:43 -0700

    Updated comment migration and seed file to implement fixed comment and vote assocations

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,4 +2,5 @@ class Proposal < ApplicationRecord
   has_many :user_proposals
   has_many :users, through: :user_proposals
   has_many :votes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- db/migrate/20160825172348_create_comments.rb
+++ db/migrate/20160825172348_create_comments.rb
@@ -1,9 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text :body, null: false
       t.belongs_to :proposal
       t.belongs_to :user
+
+      t.text :body
 
       t.timestamps null: false
     end
